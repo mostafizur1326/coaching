9994a0a99b3197ea7b0c393be18680f1294267fb
mostafizur1326 <mostafizur1326@gmail.com>
2024-11-14 10:51:12 +0500

    update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
-const connectDB =  async () => {
+const connectDB = async () => {
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI environment variable is not defined.");
+  }
+
   try {
-  const connectionITC = await mongoose.connect(`${process.env.MONGODB_URI}/firstpack`);
-  console.log(`MongoDB connected || DB HOST: ${connectionITC.connection.host}`);
+    const connectionITC = await mongoose.connect(`${process.env.MONGODB_URI}/firstpack`);
+    console.log(`MongoDB connected || DB HOST: ${connectionITC.connection.host}`);
   } catch (error) {
-    console.error('MongoDB database connection FAILED: ', error);
-    process.exit(1);
+    console.error('MongoDB connection FAILED:', error.message);
+    throw error;
   }
 };
 
